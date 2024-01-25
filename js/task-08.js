a4443a4feb3633a8ff1ca165c9af0392c773a5b5
Wiola-D <wioletta.d.work@gmail.com>
2024-01-25 20:41:25 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,17 +1,24 @@
 const registerForm = document.querySelector(".login-form");
 
+let formData = {};
+
 registerForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
+
   const form = event.target;
   const email = form.elements.email.value;
   const password = form.elements.password.value;
 
-  if (email === "" || password === "") {
+  if (email.trim() === "" || password.trim() === "") {
     return alert("Wszystkie pola powinny zostać wypełnione!");
   }
 
-  console.log(`{Email: ${email}} {Password: ${password}}`);
+  formData.email = email;
+  formData.password = password;
+
+  console.log(formData);
+
   form.reset();
 }
